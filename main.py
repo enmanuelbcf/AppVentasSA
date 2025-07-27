badd93f01fe78ab9871c15beb5232e7107c478e6
enmanuel <enmanuelbcf2@gmail.com>
2025-07-27 15:02:02 -0400

    chore - Arreglo la fecha de envio de cuadre

--- main.py
+++ main.py
@@ -30,5 +30,5 @@ app.include_router(Auth_router)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))  # Usa PORT de Railway o 8000 por defecto local
-    uvicorn.run(app, host="192.168.100.87", port=port)
-    # uvicorn.run(app, host="0.0.0.0", port=port)
+    # uvicorn.run(app, host="192.168.100.87", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
